ceb76e322b4abb872732a3700ec27ef48002a00c
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-11 03:48:56 -0800

    Add files via upload

---
+++ CountOfTheCharacter.java
@@ -0,0 +1,23 @@
+package assesment;
+
+public class CountOfTheCharacter {
+
+	public static void main(String[] args) {
+
+		String str = "Pikachu";
+
+		System.out.println(str.length());
+
+		System.out.println();
+		/*
+		 * for (int i = 0; i < str.length(); i++) {
+		 * 
+		 * 
+		 * System.out.println(str.charAt(i));
+		 * 
+		 * }
+		 */
+
+	}
+
+}
