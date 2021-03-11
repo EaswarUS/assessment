ace5df0a7238c0c272988b19da6f7f997d6740af
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-11 03:56:53 -0800

    Add files via upload

---
+++ ReverseOfTheString.java
@@ -0,0 +1,14 @@
+package assesment;
+
+public class ReverseOfTheString {
+
+	public static void main(String[] args) {
+		String str = "WELCOME";
+		for (int i = str.length()-1; i>=0 ; i--) {
+			System.out.println(str.charAt(i));
+			
+		}
+
+	}
+
+}
